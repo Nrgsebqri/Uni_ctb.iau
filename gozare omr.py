379e5b443fe5254ec1bbb1f0be2c8a24e1696c64
Nrgsebqri <154313470+Nrgsebqri@users.noreply.github.com>
2024-03-24 21:03:05 +0330

    Add files via upload

---
+++ gozare omr.py
@@ -0,0 +1,11 @@
+
+yearborn = int(input())
+yearnow = int(input())
+
+yearsold = (yearnow - yearborn)
+print(yearsold , "years old")
+print(yearsold * 12 , " month old")
+print(yearsold * 365 , "day old")
+print(yearsold * 8760 , "hour old")
+print(yearsold * 525960 , "minute old" )
+print(yearsold * 31557600 , "second old" )
